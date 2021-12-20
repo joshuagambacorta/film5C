7b7a79c9dc1d8050c776a280d245460f29795c6a
joshua <joshua.gambacorta@gmail.com>
2021-12-20 22:41:01 +0100

    Complete the pass value but Toast not works.

--- app/src/main/java/com/example/film5c/MainActivity.java
+++ app/src/main/java/com/example/film5c/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.film5c;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    String film[] = {"Spiderman", "Titanic", "Catwoman", "Superman"};
+    ListView lvFilm;
+    Button btnEsci;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvFilm = (ListView)findViewById(R.id.lvFilm);
+        ArrayAdapter<String> aaFilm = new ArrayAdapter<String>(
+                this, android.R.layout.simple_expandable_list_item_1, film
+        );
+
+        lvFilm.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String titolo = (String)lvFilm.getItemAtPosition(position);
+                Intent i = new Intent(getApplicationContext(), MainActivity2.class);
+                i.putExtra("etichetta", "ciao");
+                startActivity(i);
+                Log.d("click_provdedura", "Hai cliccato sulla " + position + " voce.");
+            }
+        });
+
+        lvFilm.setAdapter(aaFilm);
+
+        btnEsci = (Button)findViewById(R.id.btnesci);
+
+        btnEsci.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "prova", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/film5c/MainActivity2.java
+++ app/src/main/java/com/example/film5c/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.film5c;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Intent i = getIntent();
+        String testoStampa=i.getStringExtra("etichetta");
+        Toast t = Toast.makeText(getApplicationContext(), testoStampa,Toast.LENGTH_LONG );
+        t.show();
     }
 }
